f75196cc21a68f846bbd2725dce13204bc110034
PEDRO AUGUSTO BARBOSA DE SOUZA RUIZ <pedro.ruiz>
2026-04-01 20:40:33 -0300

    01/04

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,15 @@ import { Routes, Route } from 'react-router-dom';
 
 //import Home from './pages/home';
 
-import AvaliacaoMotorista from '.src/pages/avaliacaoMotorista';
+import AvaliacaoMotorista from './pages/avaliacaoMotorista';
 import CadastroMotorista from './pages/cadMotorista/index';
 import EditarPontos from './pages/editarPontos';
-import EditarRota from './pages/exemplos/ex-05';
-import Home from './pages/';
-import HomeAdm from './pages/exemplos/ex-07';
-import pontos from './pages/exemplos/ex-08';
-import rotasLinhas from './pages/exemplos/ex-09';
+import EditarRota from './pages/editarRota';
+import Home from './pages/home';
+import HomeAdm from './pages/homeAdm';
+import CadastroPontos from './pages/pontos';
+import RotasLinhas from './pages/rotasLinhas';
+import Horarios from './pages/horarios';
 
 
 
@@ -19,26 +20,14 @@ function App() {
   <Routes>
     <Route path="/" element={<Home />} />
 
-    {/*<Route path="/exemplo/1" element={<Exemplo01 />} />
-    <Route path="/exemplo/2" element={<Exemplo02 />} />
-    <Route path="/exemplo/3" element={<Exemplo03 />} />
-    <Route path="/exemplo/4" element={<Exemplo04 />} />    
-    <Route path="/exemplo/5" element={<Exemplo05 />} />
-    <Route path="/exemplo/6" element={<Exemplo06 />} />
-    <Route path="/exemplo/7" element={<Exemplo07 />} />
-    <Route path="/exemplo/8" element={<Exemplo08 />} />
-    <Route path="/exemplo/9" element={<Exemplo09 />} />
-    <Route path="/exemplo/10" element={<Exemplo10 />} />
-    <Route path="/exemplo/11" element={<Exemplo11 />} />
-    <Route path="/exemplo/12" element={<Exemplo12 />} />
-
-
-    <Route path="/atividade/1" element={<Atividade01 />} />
-    <Route path="/atividade/2" element={<Atividade02 />} />
-    <Route path="/atividade/3" element={<Atividade03 />} />
-    <Route path="/atividade/4" element={<Atividade04 />} />
-    <Route path="/atividade/5" element={<Atividade05 />} />
-    <Route path="/atividade/6" element={<Atividade06 />} />*/}
+    <Route path="/avaliacao" element={<AvaliacaoMotorista />} />
+    <Route path="/cadmotora" element={<CadastroMotorista />} />
+    <Route path="/editarpontos" element={<EditarPontos />} />
+    <Route path="/editarrota" element={<EditarRota />} />
+    <Route path="/adm" element={<HomeAdm />} />
+    <Route path="/cadpontos" element={<CadastroPontos />} />
+    <Route path="/rotas" element={<RotasLinhas />} />
+    <Route path="/horarios" element={<Horarios />} />
     
   </Routes>
   )
--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,8 +1,52 @@
+import { Link } from "react-router";
+
+import './styles.css'
+
 export default function Home() {
     return (
-        <>
+        <div className="container">
+      <header className="header">
+        <h1>TRANSPORTE PÚBLICO</h1>
 
-            <h1>Home</h1>
-        </>
+        <nav className="menu">
+          <Link to='/adm'>
+          <button className="admin">Administrador</button>
+          </Link>
+        </nav>
+      </header>
+
+      <section className="banner" />
+
+      <section className="cards">
+        <div className="card">
+          <Link to='/rotas'>
+          <img src="https://www.figma.com/api/mcp/asset/74a470de-ceb1-4aaa-a213-e6e3c40a7789" />
+          <h3>Pontos de Ônibus</h3>
+          <p>Encontre os pontos de ônibus mais próximos.</p>
+          <button className="green">Ver Pontos</button>
+          </Link>
+        </div>
+
+        <div className="card">
+          <Link to='/rotas'>
+          <img src="https://www.figma.com/api/mcp/asset/f1d4ce75-9bb6-4f66-98cb-8485f76220bd" />
+          <h3>Rotas de Ônibus</h3>
+          <p>Planeje sua rota e veja o melhor trajeto.</p>
+          <button className="blue">Ver Rotas</button>
+          </Link>
+        </div>
+
+        <div className="card">
+          <Link to='/horarios'>
+          <img src="https://www.figma.com/api/mcp/asset/6706dbc5-7975-426a-8a87-793eb120beb3" />
+          <h3>Horários de Ônibus</h3>
+          <p>Consulte os horários de saída e chegada.</p>
+          <button className="orange">Ver Horários</button>
+          </Link>
+        </div>
+      </section>
+
+      <footer className="footer" />
+    </div>
     )
 }
